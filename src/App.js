b18f88a0f561b1e246e391ec3d3fee2022fe1b7f
Milli-saved <million12.tenkir@gmail.com>
2023-03-21 02:19:20 +0300

    Working very well

--- src/App.js
+++ src/App.js
@@ -3,11 +3,28 @@ import "./App.css";
 import Loader from "./Globals/Loader";
 
 const App = () => {
+  const handleClicker = () => {
+    console.log("clicked");
+  };
   const name = "Fanuel H.";
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <Loader />
+        <button onClick={handleClicker}>Hellow</button>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p>{name}</p>
       </header>
     </div>
   );
